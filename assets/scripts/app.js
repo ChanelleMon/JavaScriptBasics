5af7b8849728f6aa878ec030f58fb563446a714a
Chanelle <chanellemon@hotmail.com>
2023-06-13 16:06:17 -0400

    Strings section

---
+++ assets/scripts/app.js
@@ -0,0 +1,9 @@
+const defaultResult = 0;
+
+let currentResult = defaultResult;
+
+currentResult = currentResult + 10;
+
+let calcDesc = `(${defaultResult} + 10)`
+
+outputResult(currentResult, calcDesc);
